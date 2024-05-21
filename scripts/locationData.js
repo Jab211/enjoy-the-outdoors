0d67c5e8bbfebe10e2bfde06c37a6c679a66968a
Jason <jasonbalderrama6@gmail.com>
2024-05-21 11:26:20 -0400

    moving code around

--- scripts/locationData.js
+++ scripts/locationData.js
@@ -1,4 +1,4 @@
-"use strict";
+
 const locationsArray = [
   "Alabama",
   "Alaska",
@@ -57,47 +57,3 @@ const locationsArray = [
   "Wyoming",
 ];
 
-window.onload = function () {
-  // HTML element variables
-  const locationButton = document.querySelector("#locationButton");
-  const parkTypeButton = document.querySelector("#parkTypeButton");
-
-  const locationContainer = document.querySelector("#locationContainer");
-  const parkTypeContainer = document.querySelector("#parkTypeContainer");
-
-  const locationSelect = document.querySelector("#locationSelect");
-
-  function searchTypeChanged() {
-    if (locationButton.checked) {
-      locationContainer.style.display = "block";
-    } else {
-      locationContainer.style.display = "none";
-    }
-    if (parkTypeButton.checked) {
-      parkTypeContainer.style.display = "block";
-    } else {
-      parkTypeContainer.style.display = "none";
-    }
-  }
-
-  function loadStates() {
-    for (const location of locationsArray) {
-      const option = document.createElement("option");
-      option.innerText = location;
-      locationSelect.appendChild(option);
-    }
-  }
-  locationButton.addEventListener("change", searchTypeChanged);
-  parkTypeButton.addEventListener("change", searchTypeChanged);
-//   locationButton.onchange = searchTypeChanged;
-//   parkTypeButton.onchange = searchTypeChanged;
-  searchTypeChanged();
-  
-
-  //functions
-
-  //event handling
-
-  //initial loading
-  loadStates();
-};
--- scripts/national-parks.js
+++ scripts/national-parks.js
@@ -1,6 +1,59 @@
+"use strict"
+// function loadParks() {
+//   for (const park of nationalParksArray) {
+//   }
+// }
 
-function loadParks() {
-  for (const park of nationalParksArray) {
+window.onload = function () {
+  // HTML element variables
+  const locationButton = document.querySelector("#locationButton");
+  const parkTypeButton = document.querySelector("#parkTypeButton");
+
+  const locationContainer = document.querySelector("#locationContainer");
+  const parkTypeContainer = document.querySelector("#parkTypeContainer");
+
+  const locationSelect = document.querySelector("#locationSelect");
+
+  function searchTypeChanged() {
+    if (locationButton.checked) {
+      locationContainer.style.display = "block";
+      loadStates()
+    } else {
+      locationContainer.style.display = "none";
+    }
+    if (parkTypeButton.checked) {
+      parkTypeContainer.style.display = "block";
+      loadParks();
+    } else {
+      parkTypeContainer.style.display = "none";
+    }
   }
-}
 
+  function loadStates() {
+    for (const location of locationsArray) {
+      const option = document.createElement("option");
+      option.innerText = location;
+      locationSelect.appendChild(option);
+    }
+  }
+  function loadParks() {
+        for (const park of parkTypesArray) {
+          const option = document.createElement("option");
+          option.innerText = park;
+          typeSelect.appendChild(option);
+        }
+      }
+  locationButton.addEventListener("change", searchTypeChanged);
+  parkTypeButton.addEventListener("change", searchTypeChanged);
+//   locationButton.onchange = searchTypeChanged;
+//   parkTypeButton.onchange = searchTypeChanged;
+  searchTypeChanged();
+  
+
+  //functions
+
+  //event handling
+
+  //initial loading
+  // loadStates();
+};
